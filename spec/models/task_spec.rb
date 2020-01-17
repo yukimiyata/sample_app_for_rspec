f07382a69c6d3b39dc8daad5978a8596b17adc1a
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-01-17 18:10:10 +0900

    エラーメッセージをテストに含めました。改行を直しました。

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,35 +5,38 @@ RSpec.describe Task, type: :model do
     let(:task) { create(:task) }
 
     it 'is valid with a title and status' do
-        expect(task).to be_valid
+      expect(task).to be_valid
     end
 
     it 'is invalid without a title' do
-        task.title = nil
-        expect(task).to_not be_valid
+      task.title = nil
+      expect(task).to_not be_valid
+      expect(task.errors[:title]).to include("can't be blank")
     end
 
     it 'is invalid without status' do
-        task.status = nil
-        expect(task).to_not be_valid
+      task.status = nil
+      expect(task).to_not be_valid
+      expect(task.errors[:status]).to include("can't be blank")
     end
 
     it 'is invalid with a duplicate title' do
-        task
-        another_task = Task.new(
-            title: "テストタイトル",
-            status: "doing"
-        )
-        expect(another_task).to_not be_valid
+      task
+      another_task = Task.new(
+          title: "テストタイトル",
+          status: "doing"
+      )
+      expect(another_task).to_not be_valid
+      expect(another_task.errors[:title]).to include("has already been taken")
     end
 
     it "is valid with a another title" do
-        task
-        another_task = Task.new(
-            title: "テストタイトル２",
-            status: 0
-        )
-        expect(another_task).to be_valid
+      task
+      another_task = Task.new(
+          title: "テストタイトル２",
+          status: 0
+      )
+      expect(another_task).to be_valid
     end
   end
 end
