efc292f78d34d556a3b82981848e19f0ff069cd6
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-01-17 17:54:54 +0900

    validationに名前を変更して、contextを削除しました。

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,44 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  describe '.valid?' do
+  describe 'validation' do
     let(:task) { create(:task) }
-    context 'valid case ' do
-      it 'is valid with a title and status' do
+
+    it 'is valid with a title and status' do
         expect(task).to be_valid
-      end
     end
 
-    context 'invalid case' do
-      it 'is invalid without a title' do
+    it 'is invalid without a title' do
         task.title = nil
         expect(task).to_not be_valid
-      end
-
-      it 'is invalid without status' do
-        task.status = nil
-        expect(task).to_not be_valid
-      end
     end
 
-    context 'duplicate title' do
-      it 'is invalid with a duplicate title' do
+    it 'is invalid without status' do
+        task.status = nil
+        expect(task).to_not be_valid
+    end
+
+    it 'is invalid with a duplicate title' do
         task
         another_task = Task.new(
             title: "テストタイトル",
             status: 1
         )
         expect(another_task).to_not be_valid
-      end
+    end
 
-      it "is valid with a another title" do
+    it "is valid with a another title" do
         task
         another_task = Task.new(
             title: "テストタイトル２",
             status: 0
         )
         expect(another_task).to be_valid
-      end
     end
   end
 end
