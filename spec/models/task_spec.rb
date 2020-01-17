b2f0277b163f82cf3dd5a4dcd6b20be31872121a
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-01-17 16:54:15 +0900

    rspecを記述しました。

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.valid?' do
+    let(:task) { create(:task) }
+    context 'valid case ' do
+      it 'is valid with a title and status' do
+        expect(task).to be_valid
+      end
+    end
+
+    context 'invalid case' do
+      it 'is invalid without a title' do
+        task.title = nil
+        expect(task).to_not be_valid
+      end
+
+      it 'is invalid without status' do
+        task.status = nil
+        expect(task).to_not be_valid
+      end
+    end
+
+    context 'duplicate title' do
+      it 'is invalid with a duplicate title' do
+        task
+        another_task = Task.new(
+            title: "テストタイトル",
+            status: 1
+        )
+        expect(another_task).to_not be_valid
+      end
+
+      it "is valid with a another title" do
+        task
+        another_task = Task.new(
+            title: "テストタイトル２",
+            status: 0
+        )
+        expect(another_task).to be_valid
+      end
+    end
+  end
 end
