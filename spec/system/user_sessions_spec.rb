322275a2ab8a3e9c20ced8340881d7e6116b0cdc
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-01-20 12:31:09 +0900

    add: ログインに関するsystemスペックを追加

---
+++ spec/system/user_sessions_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'UserSessions', type: :system do
+  let(:user) { create(:user) }
+  describe 'login' do
+    context ' success' do
+      it 'is valid to login' do
+        user
+        visit login_path
+        fill_in 'Email', with: 'testsample@gmail.com'
+        fill_in 'Password', with: 'password'
+        click_button 'Login'
+        expect(page).to have_content 'Login successful'
+        expect(current_path).to eq root_path
+      end
+    end
+
+    context 'fail' do
+      it 'is invalid to login without value' do
+        visit login_url
+        click_button 'Login'
+        expect(page).to have_content 'Login failed'
+      end
+    end
+  end
+end
