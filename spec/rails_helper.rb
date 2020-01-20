1439a7d519e275a2199c770b42232c3eb73e8f5c
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-01-20 12:34:02 +0900

    add: loginメソッド(spec/support以下)を読み込む設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
+  config.include AuthenticationForFeatureRequest, type: :system
+
 
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
